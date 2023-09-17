a10c586af7272e5bfa5f467f07fc3026ef5cb492
Raahim Fareed <raahim1965@hotmail.com>
2023-09-18 02:18:40 +0500

    more test routes

--- src/routes.php
+++ src/routes.php
@@ -2,5 +2,7 @@
 use Raahim\Project1\App\Route;
 
 Route::get("/", fn () => "Hello, World");
+Route::get("/post", fn () => "This is a post route");
+Route::get("/", fn () => "Line by line routes are overwritten");
 
 
