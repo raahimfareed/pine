a90d20d40670aaa0a879a1fb0418a13dce14020a
Raahim Fareed <raahim1965@hotmail.com>
2023-09-18 02:35:48 +0500

    test route for testing post

--- src/routes.php
+++ src/routes.php
@@ -5,4 +5,6 @@ Route::get("/", fn () => "Hello, World");
 Route::get("/post", fn () => "This is a post route");
 Route::get("/", fn () => "Line by line routes are overwritten");
 
+Route::post("/test", fn () => "This is a test response");
+
 
