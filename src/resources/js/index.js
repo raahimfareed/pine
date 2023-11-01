37f287a5216c221f468ed29802859c96275819a3
Raahim Fareed <raahim1965@hotmail.com>
2023-11-02 01:25:20 +0500

    basic view styles and js

--- src/resources/js/index.js
+++ src/resources/js/index.js
@@ -1,1 +1,13 @@
 console.log("Hello via Bun!");
+
+const form = document.getElementById("form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const heading = document.getElementById("toChange");
+  const name = document.getElementById("name").value.trim();
+  if (name.length === 0) {
+    heading.innerText = `Hey`;
+    return;
+  }
+  heading.innerText = `Hey, ${name}!`;
+});
