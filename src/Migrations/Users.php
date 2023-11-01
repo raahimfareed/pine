ebb7150a54087f76ada0caf1978c78f2baf9d280
Raahim Fareed <raahim1965@hotmail.com>
2023-11-02 01:24:51 +0500

    database things

--- src/Migrations/Users.php
+++ src/Migrations/Users.php
@@ -5,7 +5,6 @@ namespace Pine\App\Migrations;
 return new class {
     public function schema() {
         return [
-            "id" => "INT(11) PRIMARY KEY AUTO_INCREMENT NOT NULL",
             "name" => "VARCHAR(255) NOT NULL",
             "email" => "VARCHAR(255) NOT NULL",
             "password" => "VARCHAR(255) NOT NULL",
