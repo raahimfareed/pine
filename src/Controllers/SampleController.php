cb21fbcc114b28fa902a84b787ea1e0cff08b0cc
Raahim Fareed <raahimfareed@proton.me>
2024-03-04 22:34:55 +0500

    autoformat

--- src/Controllers/SampleController.php
+++ src/Controllers/SampleController.php
@@ -5,8 +5,10 @@ namespace Pine\Controllers;
 use Pine\App\Controller;
 use Pine\App\View;
 
-class SampleController extends Controller {
-    public function index() {
+class SampleController extends Controller
+{
+    public function index()
+    {
         return new View("controller");
     }
 }
