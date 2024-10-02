7fdffe64d17a8f0c871c54092176152ff99dea26
Raahim Fareed <raahimfareed@proton.me>
2024-10-03 04:15:12 +0500

    added headers and body, cleaned up code

--- src/App/Request.php
+++ src/App/Request.php
@@ -3,18 +3,17 @@
 namespace Pine\App;
 
 class Request {
-    private $__request_data = [];
+    public array $body = [];
+    public array $headers = [];
+
     public function __construct()
     {
+        $this->headers = getallheaders();
         $form_body = $_REQUEST;
         $entityBody = file_get_contents('php://input');
         $json_body = json_decode($entityBody, true);
 
         $final_array = [...($form_body ?? []), ...($json_body ?? [])];
-        $this->__request_data = $final_array;
-
-        foreach ($final_array as $key => $value) {
-            $this->{$key} = $value;
-        }
+        $this->body = $final_array;
     }
 }
