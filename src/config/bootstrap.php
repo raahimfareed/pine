2d6d35fe0f30d7cea37921f601ea7bca34430390
Raahim Fareed <raahimfareed@proton.me>
2024-10-06 03:04:39 +0500

    added custom exception handler

--- src/config/bootstrap.php
+++ src/config/bootstrap.php
@@ -1,4 +1,8 @@
 <?php
 
+use Pine\App\ErrorHandler;
+
+set_exception_handler([ErrorHandler::class, 'handleException']);
+
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/../..");
 $dotenv->safeLoad();
