c2a268cb40ba4e3cdefe4f5eb5869e9a79b21b2e
Raahim Fareed <raahimfareed@proton.me>
2024-10-06 03:09:25 +0500

    added env function to get variables from env and type cast them

--- src/App/Util.php
+++ src/App/Util.php
@@ -7,4 +7,25 @@ class Util {
         json_decode($json);
         return json_last_error() == JSON_ERROR_NONE;
     }
+
+    public static function env($key, $default = null) {
+        $value = $_ENV[$key] ?? $default;
+
+        if ($value === null) {
+            return $default;
+        }
+
+        if ($value === "true") {
+            return true;
+        }
+        if ($value === "false") {
+            return false;
+        }
+
+        if (is_numeric($value)) {
+            return str_contains($value, ".") ? (float) $value : (int) $value;
+        }
+
+        return $value;
+    }
 }
