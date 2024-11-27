13c46e8f77796c752b17f7a7069e68747826325a
Raahim Fareed <raahimfareed@proton.me>
2024-11-27 13:13:27 +0500

    a sample user model

---
+++ src/Models/User.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Pine\Models;
+
+use Pine\App\BaseModel;
+
+class User extends BaseModel {
+    public static string $_table = "users";
+    public static string $_primaryKey = "id";
+    public static array $_attributes = [];
+
+    public string $name;
+    public string $email;
+    private string $password;
+
+    public static function initialize() {
+        self::setup()
+            ->addColumn('id', 'INT')->primary('id')->autoIncrement('id')
+            ->addColumn('name', "VARCHAR(255)")->nullable('name', false)
+            ->addColumn('email', "VARCHAR(255)")->nullable('email', false)
+            ->addColumn('password', "VARCHAR(255)")->nullable('password', false);
+    }
+}
